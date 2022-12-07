1b48c91dcdd712a2d8b56cefbc4df3a1b64a8480
cafemanager <cafemanager97@gmail.com>
2022-12-06 23:25:53 -0800

    finish assignment

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,13 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  let x = usersArray.find(function(val){
+    return val['username']===username
+  })
+
+  return x
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +33,23 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  
+  usersArray.map(function(e){console.log(e)})
+  let x = usersArray.findIndex(function(val){
+    return val['username'] === username
+  })
+  if(x === -1){
+    console.log("undefined")
+    return undefined;
+  }
+
+  let hold = usersArray[x]
+  
+  let left = usersArray.splice(0,x)
+  let right = usersArray.splice(0,x+1)
+  usersArray = left.concat(right)
+
+  return hold
+
+}
